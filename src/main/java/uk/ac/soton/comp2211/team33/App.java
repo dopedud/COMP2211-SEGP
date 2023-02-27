4e22b954750a5709e771cc8220243c02da5597fa
dopedud <mabs1u21@soton.ac.uk>
2023-02-27 16:52:24 +0000

    Change some formatting 2.

--- src/main/java/uk/ac/soton/comp2211/team33/App.java
+++ src/main/java/uk/ac/soton/comp2211/team33/App.java
@@ -1,11 +1,12 @@
 package uk.ac.soton.comp2211.team33;
 
+import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
-public class Application extends javafx.application.Application {
+public class App extends Application {
   public static void main(String[] args) {
     launch();
   }
