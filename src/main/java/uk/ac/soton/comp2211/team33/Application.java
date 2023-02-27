44ee1e2058a9691d0db60defbd9d5aeca9ed3b26
dopedud <mabs1u21@soton.ac.uk>
2023-02-27 16:40:07 +0000

    Change some formatting.

--- src/main/java/uk/ac/soton/comp2211/team33/Application.java
+++ src/main/java/uk/ac/soton/comp2211/team33/Application.java
@@ -6,17 +6,16 @@ import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class Application extends javafx.application.Application {
-    @Override
-    public void start(Stage stage) {
-        String javaVersion = System.getProperty("java.version");
-        String javafxVersion = System.getProperty("javafx.version");
-        Label l = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
-        Scene scene = new Scene(new StackPane(l), 640, 480);
-        stage.setScene(scene);
-        stage.show();
-    }
-
-    public static void main(String[] args) {
-        launch();
-    }
+  public static void main(String[] args) {
+    launch();
+  }
+  @Override
+  public void start(Stage stage) {
+    String javaVersion = System.getProperty("java.version");
+    String javafxVersion = System.getProperty("javafx.version");
+    Label l = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
+    Scene scene = new Scene(new StackPane(l), 640, 480);
+    stage.setScene(scene);
+    stage.show();
+  }
 }
