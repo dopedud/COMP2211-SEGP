5f2fbb2c4522b96be9fa679761265aced24abca1
Brian Le <brianle1301@outlook.com>
2023-02-27 02:02:13 +0000

    Add .idea folder

--- src/main/java/seg/team33/Launcher.java
+++ src/main/java/seg/team33/Launcher.java
@@ -1,7 +1,5 @@
-package seg.team33;
-
 public class Launcher {
-  public static void Main(String[] args){
+  public static void Main(String[] args) {
     System.out.println("Hello World!");
   }
 }
