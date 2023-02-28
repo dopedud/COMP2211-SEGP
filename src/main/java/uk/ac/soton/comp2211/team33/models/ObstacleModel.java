b5813180720b928d645649b4efe5996d1120a503
dopedud <mabs1u21@soton.ac.uk>
2023-02-28 16:17:20 +0000

    Rename class from Obstacle to ObstacleModel.

--- src/main/java/uk/ac/soton/comp2211/team33/models/ObstacleModel.java
+++ src/main/java/uk/ac/soton/comp2211/team33/models/ObstacleModel.java
@@ -1,11 +1,11 @@
 package uk.ac.soton.comp2211.team33.models;
 
-public class Obstacle {
+public class ObstacleModel {
   private String name;
   private int height;
   private int length;
 
-  public Obstacle(String name, int height, int length) {
+  public ObstacleModel(String name, int height, int length) {
     this.name = name;
     this.height = height;
     this.length = length;
