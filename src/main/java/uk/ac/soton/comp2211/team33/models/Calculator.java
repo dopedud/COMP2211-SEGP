b123637c74462fa22484cc2ebea9aac3ab98e320
Jackson <jl14u21@soton.ac.uk>
2023-02-28 16:29:54 +0000

    Added Runway object, also added calculator class

---
+++ src/main/java/uk/ac/soton/comp2211/team33/models/Calculator.java
@@ -0,0 +1,11 @@
+package uk.ac.soton.comp2211.team33.models;
+
+public class Calculator {
+
+    public static void main(String[] args) {
+
+    }
+
+    //Function that calculates the take-off runway available
+    private int tora ()
+}
---
+++ src/main/java/uk/ac/soton/comp2211/team33/models/Runway.java
@@ -0,0 +1,93 @@
+package uk.ac.soton.comp2211.team33.models;
+
+public class Runway {
+    private String rdesignator;
+    //Initial values of the runway
+    private Double tora, toda, asda, lda, resa, als, tocs;
+    //Currently used runway values (after calculation)
+    private Double ctora, ctoda, casda, clda, cresa, cals, ctocs;
+    private Double threshold, clearway, stopway, stripEnd, blastProtection;
+    private Double strip;
+
+    public Runway(String rdesignator, Double tora, Double toda, Double asda, Double lda, Double resa, Double als,
+                  Double tocs, Double threshold, Double clearway, Double stopway, Double stripEnd, Double blastProtection, Double strip) {
+        this.rdesignator = rdesignator;
+        this.tora = tora;
+        this.toda = toda;
+        this.asda = asda;
+        this.lda = lda;
+        this.resa = resa;
+        this.als = als;
+        this.tocs = tocs;
+        this.ctora = tora;
+        this.ctoda = toda;
+        this.casda = asda;
+        this.clda = lda;
+        this.cresa = resa;
+        this.cals = als;
+        this.ctocs = tocs;
+        this.threshold = threshold;
+        this.clearway = clearway;
+        this.stopway = stopway;
+        this.stripEnd = stripEnd;
+        this.blastProtection = blastProtection;
+        this.strip = strip;
+    }
+
+    //Getters and setter for all current values that can be changed by a re-declaration
+    public Double getCtora() {
+        return ctora;
+    }
+
+    public void setCtora(Double ctora) {
+        this.ctora = ctora;
+    }
+
+    public Double getCtoda() {
+        return ctoda;
+    }
+
+    public void setCtoda(Double ctoda) {
+        this.ctoda = ctoda;
+    }
+
+    public Double getCasda() {
+        return casda;
+    }
+
+    public void setCasda(Double casda) {
+        this.casda = casda;
+    }
+
+    public Double getClda() {
+        return clda;
+    }
+
+    public void setClda(Double clda) {
+        this.clda = clda;
+    }
+
+    public Double getCresa() {
+        return cresa;
+    }
+
+    public void setCresa(Double cresa) {
+        this.cresa = cresa;
+    }
+
+    public Double getCals() {
+        return cals;
+    }
+
+    public void setCals(Double cals) {
+        this.cals = cals;
+    }
+
+    public Double getCtocs() {
+        return ctocs;
+    }
+
+    public void setCtocs(Double ctocs) {
+        this.ctocs = ctocs;
+    }
+}
