78dac2b2726e5130b62797bc47210da02954aa87
dopedud <mabs1u21@soton.ac.uk>
2023-02-28 18:39:41 +0000

    Merged branch Jackson into Abeed, added some Javadoc comments.

--- src/main/java/uk/ac/soton/comp2211/team33/models/Calculator.java
+++ src/main/java/uk/ac/soton/comp2211/team33/models/Calculator.java
@@ -1,8 +1,6 @@
 package uk.ac.soton.comp2211.team33.models;
 
 public class Calculator {
-
-
     //Function that calculates the take-off runway available
     // TORA for Take Off Away and Landing Over
     private double tora1 (Runway runway, double distanceThreshold, double displacedThreshold){
