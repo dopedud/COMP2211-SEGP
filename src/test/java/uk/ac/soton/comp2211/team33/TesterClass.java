91ccf37ca97b538339a23b28c5845fa5e44168c6
Jackson <jl14u21@soton.ac.uk>
2023-03-08 12:13:35 +0000

    Minor adjustments on UI

---
+++ src/test/java/uk/ac/soton/comp2211/team33/TesterClass.java
@@ -0,0 +1,12 @@
+package uk.ac.soton.comp2211.team33;
+
+import uk.ac.soton.comp2211.team33.models.Runway;
+
+public class TesterClass {
+
+  public static void main(String[] args) {
+    Runway runway = new Runway("01L", 3660,3660,3660,3353,240,307);
+    System.out.println(runway);
+  }
+
+}
