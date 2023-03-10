34c08eea6559afbd2cb9461657ce61aea67be1ff
dopedud <mabs1u21@soton.ac.uk>
2023-03-10 13:53:22 +0000

    Re-wrote MainScene and RunwayScene.

---
+++ src/main/java/uk/ac/soton/comp2211/team33/scenes2/MainScene.java
@@ -0,0 +1,34 @@
+package uk.ac.soton.comp2211.team33.scenes2;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TabPane;
+import javafx.stage.Stage;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import uk.ac.soton.comp2211.team33.models.Airport;
+
+public class MainScene extends BaseScene {
+
+  private static final Logger logger = LogManager.getLogger(MainScene.class);
+
+//  @FXML
+//  private TabPane runwayTabs;
+
+  public MainScene(Stage stage, Airport state) {
+    super(stage, state);
+  }
+
+  @Override
+  protected void build() {
+    logger.info("Building MainScene...");
+
+    stage.setTitle("Runway Re-decleration Tool - " + state.getName());
+
+    renderFXML("MainScene.fxml");
+  }
+
+  @FXML
+  private void onNewRunway() {
+    new NewRunwayScene(createModalStage(), state);
+  }
+}
---
+++ src/main/java/uk/ac/soton/comp2211/team33/scenes2/NewRunwayScene.java
@@ -0,0 +1,18 @@
+package uk.ac.soton.comp2211.team33.scenes2;
+
+import javafx.stage.Stage;
+import uk.ac.soton.comp2211.team33.models.Airport;
+
+public class NewRunwayScene extends BaseScene {
+
+  public NewRunwayScene(Stage stage, Airport state) {
+    super(stage, state);
+  }
+
+  @Override
+  protected void build() {
+    stage.setTitle("New Runway");
+
+    renderFXML("NewRunwayScene.fxml");
+  }
+}
