244f274ef3d251fe176ff674cafafc1ede33a975
dopedud <mabs1u21@soton.ac.uk>
2023-03-15 23:37:39 +0000

    Added more tests to test for obstacle exceeding maximum center-line and distance threshold.

--- src/test/java/uk/ac/soton/comp2211/team33/CalculatorTest.java
+++ src/test/java/uk/ac/soton/comp2211/team33/CalculatorTest.java
@@ -3,6 +3,7 @@ package uk.ac.soton.comp2211.team33;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -21,7 +22,7 @@ public class CalculatorTest {
 
   private static Runway runway09L, runway27R, runway09R, runway27L;
 
-  private static Obstacle obstacle12H0N, obstacle25H20S, obstacle15H60N, obstacle20H20N;
+  private static Obstacle obstacle12H0N, obstacle25H20S, obstacle15H60N, obstacle20H20N, obstacle20H100S;
 
   private static Aircraft aircraft;
 
@@ -37,8 +38,18 @@ public class CalculatorTest {
     obstacle25H20S = new Obstacle("BrokenAirplane", 25, 0, -20);
     obstacle15H60N = new Obstacle("BrokenAirplane", 15, 0, 60);
     obstacle20H20N = new Obstacle("BrokenAirplane", 20, 0, 20);
+    obstacle20H100S = new Obstacle("BrokenAirplane", 20, 0, -100);
 
     aircraft = new Aircraft("A380", 300);
+  }
+
+  @BeforeEach
+  @DisplayName("Reset calculations for runway values.")
+  public void resetCalculations() {
+    Calculator.resetCalculationsPP(runway09L);
+    Calculator.resetCalculationsPP(runway27R);
+    Calculator.resetCalculationsPP(runway09R);
+    Calculator.resetCalculationsPP(runway27L);
   }
 
   @Test
@@ -47,9 +58,9 @@ public class CalculatorTest {
     runway27R.setObsDistFromThresh(3646);
     Calculator.takeOffTowardsObsPP(runway27R, obstacle12H0N);
 
-    assertEquals(2986, runway27R.getCtora(), "Calculated TORA of runway is incorrect after calculation.");
-    assertEquals(2986, runway27R.getCtoda(), "Calculated TODA of runway is incorrect after calculation.");
-    assertEquals(2986, runway27R.getCasda(), "Calculated ASDA of runway is incorrect after calculation.");
+    assertEquals(2986, runway27R.getCtora(), "Calculated TORA is incorrect after calculation.");
+    assertEquals(2986, runway27R.getCtoda(), "Calculated TODA is incorrect after calculation.");
+    assertEquals(2986, runway27R.getCasda(), "Calculated ASDA is incorrect after calculation.");
   }
 
   @Test
@@ -58,9 +69,9 @@ public class CalculatorTest {
     runway09L.setObsDistFromThresh(-50);
     Calculator.takeOffAwayObsPP(runway09L, obstacle12H0N, aircraft);
 
-    assertEquals(3346, runway09L.getCtora(), "Calculated TORA of runway is incorrect after calculation.");
-    assertEquals(3346, runway09L.getCtoda(), "Calculated TODA of runway is incorrect after calculation.");
-    assertEquals(3346, runway09L.getCasda(), "Calculated ASDA of runway is incorrect after calculation.");
+    assertEquals(3346, runway09L.getCtora(), "Calculated TORA is incorrect after calculation.");
+    assertEquals(3346, runway09L.getCtoda(), "Calculated TODA is incorrect after calculation.");
+    assertEquals(3346, runway09L.getCasda(), "Calculated ASDA is incorrect after calculation.");
   }
 
   @Test
@@ -69,7 +80,7 @@ public class CalculatorTest {
     runway27R.setObsDistFromThresh(3646);
     Calculator.landingTowardsObsPP(runway27R, obstacle12H0N);
 
-    assertEquals(3346, runway27R.getClda(), "Calculated LDA of runway is incorrect after calculation.");
+    assertEquals(3346, runway27R.getClda(), "Calculated LDA is incorrect after calculation.");
   }
 
   @Test
@@ -78,7 +89,7 @@ public class CalculatorTest {
     runway09L.setObsDistFromThresh(-50);
     Calculator.landingOverObsPP(runway09L, obstacle12H0N, aircraft);
 
-    assertEquals(2985, runway09L.getClda(), "Calculated LDA of runway is incorrect after calculation.");
+    assertEquals(2985, runway09L.getClda(), "Calculated LDA is incorrect after calculation.");
   }
 
   @Test
@@ -87,9 +98,9 @@ public class CalculatorTest {
     runway09R.setObsDistFromThresh(2853);
     Calculator.takeOffTowardsObsPP(runway09R, obstacle25H20S);
 
-    assertEquals(1850, runway09R.getCtora(), "Calculated TORA of runway is incorrect after calculation.");
-    assertEquals(1850, runway09R.getCtoda(), "Calculated TODA of runway is incorrect after calculation.");
-    assertEquals(1850, runway09R.getCasda(), "Calculated ASDA of runway is incorrect after calculation.");
+    assertEquals(1850, runway09R.getCtora(), "Calculated TORA is incorrect after calculation.");
+    assertEquals(1850, runway09R.getCtoda(), "Calculated TODA is incorrect after calculation.");
+    assertEquals(1850, runway09R.getCasda(), "Calculated ASDA is incorrect after calculation.");
   }
 
   @Test
@@ -98,9 +109,9 @@ public class CalculatorTest {
     runway27L.setObsDistFromThresh(500);
     Calculator.takeOffAwayObsPP(runway27L, obstacle25H20S, aircraft);
 
-    assertEquals(2860, runway27L.getCtora(), "Calculated TORA of runway is incorrect after calculation.");
-    assertEquals(2860, runway27L.getCtoda(), "Calculated TODA of runway is incorrect after calculation.");
-    assertEquals(2860, runway27L.getCasda(), "Calculated ASDA of runway is incorrect after calculation.");
+    assertEquals(2860, runway27L.getCtora(), "Calculated TORA is incorrect after calculation.");
+    assertEquals(2860, runway27L.getCtoda(), "Calculated TODA is incorrect after calculation.");
+    assertEquals(2860, runway27L.getCasda(), "Calculated ASDA is incorrect after calculation.");
   }
 
   @Test
@@ -109,7 +120,7 @@ public class CalculatorTest {
     runway09R.setObsDistFromThresh(2853);
     Calculator.landingTowardsObsPP(runway09R, obstacle25H20S);
 
-    assertEquals(2553, runway09R.getClda(), "Calculated LDA of runway is incorrect after calculation.");
+    assertEquals(2553, runway09R.getClda(), "Calculated LDA is incorrect after calculation.");
   }
 
   @Test
@@ -118,7 +129,7 @@ public class CalculatorTest {
     runway27L.setObsDistFromThresh(500);
     Calculator.landingOverObsPP(runway27L, obstacle25H20S, aircraft);
 
-    assertEquals(1850, runway27L.getClda(), "Calculated LDA of runway is incorrect after calculation.");
+    assertEquals(1850, runway27L.getClda(), "Calculated LDA is incorrect after calculation.");
   }
 
   @Test
@@ -127,9 +138,9 @@ public class CalculatorTest {
     runway27L.setObsDistFromThresh(3203);
     Calculator.takeOffTowardsObsPP(runway27L, obstacle15H60N);
 
-    assertEquals(2393, runway27L.getCtora(), "Calculated TORA of runway is incorrect after calculation.");
-    assertEquals(2393, runway27L.getCtoda(), "Calculated TODA of runway is incorrect after calculation.");
-    assertEquals(2393, runway27L.getCasda(), "Calculated ASDA of runway is incorrect after calculation.");
+    assertEquals(2393, runway27L.getCtora(), "Calculated TORA is incorrect after calculation.");
+    assertEquals(2393, runway27L.getCtoda(), "Calculated TODA is incorrect after calculation.");
+    assertEquals(2393, runway27L.getCasda(), "Calculated ASDA is incorrect after calculation.");
   }
 
   @Test
@@ -138,9 +149,9 @@ public class CalculatorTest {
     runway09R.setObsDistFromThresh(150);
     Calculator.takeOffAwayObsPP(runway09R, obstacle15H60N, aircraft);
 
-    assertEquals(2903, runway09R.getCtora(), "Calculated TORA of runway is incorrect after calculation.");
-    assertEquals(2903, runway09R.getCtoda(), "Calculated TODA of runway is incorrect after calculation.");
-    assertEquals(2903, runway09R.getCasda(), "Calculated ASDA of runway is incorrect after calculation.");
+    assertEquals(2903, runway09R.getCtora(), "Calculated TORA is incorrect after calculation.");
+    assertEquals(2903, runway09R.getCtoda(), "Calculated TODA is incorrect after calculation.");
+    assertEquals(2903, runway09R.getCasda(), "Calculated ASDA is incorrect after calculation.");
   }
 
   @Test
@@ -149,7 +160,7 @@ public class CalculatorTest {
     runway27L.setObsDistFromThresh(3203);
     Calculator.landingTowardsObsPP(runway27L, obstacle15H60N);
 
-    assertEquals(2903, runway27L.getClda(), "Calculated LDA of runway is incorrect after calculation.");
+    assertEquals(2903, runway27L.getClda(), "Calculated LDA is incorrect after calculation.");
   }
 
   @Test
@@ -158,7 +169,7 @@ public class CalculatorTest {
     runway09R.setObsDistFromThresh(150);
     Calculator.landingOverObsPP(runway09R, obstacle15H60N, aircraft);
 
-    assertEquals(2393, runway09R.getClda(), "Calculated LDA of runway is incorrect after calculation.");
+    assertEquals(2393, runway09R.getClda(), "Calculated LDA is incorrect after calculation.");
   }
 
   @Test
@@ -167,9 +178,9 @@ public class CalculatorTest {
     runway09L.setObsDistFromThresh(3546);
     Calculator.takeOffTowardsObsPP(runway09L, obstacle20H20N);
 
-    assertEquals(2792, runway09L.getCtora(), "Calculated TORA of runway is incorrect after calculation.");
-    assertEquals(2792, runway09L.getCtoda(), "Calculated TODA of runway is incorrect after calculation.");
-    assertEquals(2792, runway09L.getCasda(), "Calculated ASDA of runway is incorrect after calculation.");
+    assertEquals(2792, runway09L.getCtora(), "Calculated TORA is incorrect after calculation.");
+    assertEquals(2792, runway09L.getCtoda(), "Calculated TODA is incorrect after calculation.");
+    assertEquals(2792, runway09L.getCasda(), "Calculated ASDA is incorrect after calculation.");
   }
 
   @Test
@@ -178,9 +189,9 @@ public class CalculatorTest {
     runway27R.setObsDistFromThresh(50);
     Calculator.takeOffAwayObsPP(runway27R, obstacle20H20N, aircraft);
 
-    assertEquals(3534, runway27R.getCtora(), "Calculated TORA of runway is incorrect after calculation.");
-    assertEquals(3612, runway27R.getCtoda(), "Calculated TODA of runway is incorrect after calculation.");
-    assertEquals(3534, runway27R.getCasda(), "Calculated ASDA of runway is incorrect after calculation.");
+    assertEquals(3534, runway27R.getCtora(), "Calculated TORA is incorrect after calculation.");
+    assertEquals(3612, runway27R.getCtoda(), "Calculated TODA is incorrect after calculation.");
+    assertEquals(3534, runway27R.getCasda(), "Calculated ASDA is incorrect after calculation.");
   }
 
   @Test
@@ -189,7 +200,7 @@ public class CalculatorTest {
     runway09L.setObsDistFromThresh(3546);
     Calculator.landingTowardsObsPP(runway09L, obstacle20H20N);
 
-    assertEquals(3246, runway09L.getClda(), "Calculated LDA of runway is incorrect after calculation.");
+    assertEquals(3246, runway09L.getClda(), "Calculated LDA is incorrect after calculation.");
   }
 
   @Test
@@ -198,6 +209,50 @@ public class CalculatorTest {
     runway27R.setObsDistFromThresh(50);
     Calculator.landingOverObsPP(runway27R, obstacle20H20N, aircraft);
 
-    assertEquals(2774, runway27R.getClda(), "Calculated LDA of runway is incorrect after calculation.");
+    assertEquals(2774, runway27R.getClda(), "Calculated LDA is incorrect after calculation.");
+  }
+
+  @Test
+  @DisplayName("Test case for when the obstacle exceeds the maximum amount of distance threshold.")
+  public void exceedDistThresh() {
+    runway27L.setObsDistFromThresh(-100);
+
+    Calculator.takeOffTowardsObsPP(runway27L, obstacle12H0N);
+    Calculator.landingTowardsObsPP(runway27L, obstacle12H0N);
+
+    assertEquals(runway27L.getTora(), runway27L.getCtora(), "TORA should not be re-declared.");
+    assertEquals(runway27L.getToda(), runway27L.getCtoda(), "TODA should not be re-declared.");
+    assertEquals(runway27L.getAsda(), runway27L.getCasda(), "ASDA should not be re-declared.");
+    assertEquals(runway27L.getLda(), runway27L.getClda(), "LDA should not be re-declared.");
+
+    Calculator.takeOffAwayObsPP(runway27L, obstacle12H0N, aircraft);
+    Calculator.landingOverObsPP(runway27L, obstacle12H0N, aircraft);
+
+    assertEquals(runway27L.getTora(), runway27L.getCtora(), "TORA should not be re-declared.");
+    assertEquals(runway27L.getToda(), runway27L.getCtoda(), "TODA should not be re-declared.");
+    assertEquals(runway27L.getAsda(), runway27L.getCasda(), "ASDA should not be re-declared.");
+    assertEquals(runway27L.getLda(), runway27L.getClda(), "LDA should not be re-declared.");
+  }
+
+  @Test
+  @DisplayName("Test case for when the obstacle exceeds the maximum amount of center-line.")
+  public void exceedCenterline() {
+    runway27L.setObsDistFromThresh(0);
+
+    Calculator.takeOffTowardsObsPP(runway27L, obstacle20H100S);
+    Calculator.landingTowardsObsPP(runway27L, obstacle20H100S);
+
+    assertEquals(runway27L.getTora(), runway27L.getCtora(), "TORA should not be re-declared.");
+    assertEquals(runway27L.getToda(), runway27L.getCtoda(), "TODA should not be re-declared.");
+    assertEquals(runway27L.getAsda(), runway27L.getCasda(), "ASDA should not be re-declared.");
+    assertEquals(runway27L.getLda(), runway27L.getClda(), "LDA should not be re-declared.");
+
+    Calculator.takeOffAwayObsPP(runway27L, obstacle20H100S, aircraft);
+    Calculator.landingOverObsPP(runway27L, obstacle20H100S, aircraft);
+
+    assertEquals(runway27L.getTora(), runway27L.getCtora(), "TORA should not be re-declared.");
+    assertEquals(runway27L.getToda(), runway27L.getCtoda(), "TODA should not be re-declared.");
+    assertEquals(runway27L.getAsda(), runway27L.getCasda(), "ASDA should not be re-declared.");
+    assertEquals(runway27L.getLda(), runway27L.getClda(), "LDA should not be re-declared.");
   }
 }
